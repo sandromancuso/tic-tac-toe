0cbce86277b70e795660267dd66f6a2abcf77921
Sandro Mancuso <sandromancuso@yahoo.com>
2013-05-28 00:46:01 +0100

    Diagonals added to winner lines

--- java/src/main/java/org/craftedsw/tictactoe/BoardLines.java
+++ java/src/main/java/org/craftedsw/tictactoe/BoardLines.java
@@ -3,20 +3,25 @@ package org.craftedsw.tictactoe;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.craftedsw.tictactoe.Board.*;
+
 class BoardLines {
 
-    private static final Line ROW_1 = new Line(Board.CELL_1, Board.CELL_2, Board.CELL_3);
-    private static final Line ROW_2 = new Line(Board.CELL_4, Board.CELL_5, Board.CELL_6);
-    private static final Line ROW_3 = new Line(Board.CELL_7, Board.CELL_8, Board.CELL_9);
+    private static final Line ROW_1 = new Line(CELL_1, CELL_2, CELL_3);
+    private static final Line ROW_2 = new Line(CELL_4, CELL_5, CELL_6);
+    private static final Line ROW_3 = new Line(CELL_7, CELL_8, CELL_9);
 
-    private static final Line COLUM_1 = new Line(Board.CELL_1, Board.CELL_4, Board.CELL_7);
-    private static final Line COLUM_2 = new Line(Board.CELL_2, Board.CELL_5, Board.CELL_8);
-    private static final Line COLUM_3 = new Line(Board.CELL_3, Board.CELL_6, Board.CELL_9);
+    private static final Line COLUM_1 = new Line(CELL_1, CELL_4, CELL_7);
+    private static final Line COLUM_2 = new Line(CELL_2, CELL_5, CELL_8);
+    private static final Line COLUM_3 = new Line(CELL_3, CELL_6, CELL_9);
+
+    private static final Line DIAGONAL_1 = new Line(CELL_1, CELL_5, CELL_9);
+    private static final Line DIAGONAL_2 = new Line(CELL_3, CELL_5, CELL_7);
 
     private List<Line> boardLines = new ArrayList<Line>();
 
     public BoardLines() {
-        add(ROW_1, ROW_2, ROW_3, COLUM_1, COLUM_2, COLUM_3);
+        add(ROW_1, ROW_2, ROW_3, COLUM_1, COLUM_2, COLUM_3, DIAGONAL_1, DIAGONAL_2);
     }
 
     private void add(Line... lines) {
---
+++ java/src/test/java/org/craftedsw/tictactoe/BoardLinesShould.java
@@ -0,0 +1,58 @@
+package org.craftedsw.tictactoe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class BoardLinesShould {
+
+    @Parameters(name = "{index}: Winner line {1} ")
+    public static Iterable<Object[]> marks() {
+        return Arrays.asList(new Object[][] {
+                {new String[] {" ", " ", " ", " ", " ", " ", " ", " ", " "}, FALSE},
+                {new String[] {"X", "X", "X", " ", " ", " ", " ", " ", " "}, TRUE},
+                {new String[] {"0", "0", "0", " ", " ", " ", " ", " ", " "}, TRUE},
+                {new String[] {" ", " ", " ", "X", "X", "X", " ", " ", " "}, TRUE},
+                {new String[] {" ", " ", " ", "0", "0", "0", " ", " ", " "}, TRUE},
+                {new String[] {" ", " ", " ", " ", " ", " ", "X", "X", "X"}, TRUE},
+                {new String[] {" ", " ", " ", " ", " ", " ", "0", "0", "0"}, TRUE},
+                {new String[] {"X", " ", " ", " ", "X", " ", " ", " ", "X"}, TRUE},
+                {new String[] {"0", " ", " ", " ", "0", " ", " ", " ", "0"}, TRUE},
+                {new String[] {" ", " ", "X", " ", "X", " ", "X", " ", " "}, TRUE},
+                {new String[] {" ", " ", "0", " ", "0", " ", "0", " ", " "}, TRUE},
+                {new String[] {"X", " ", " ", "X", " ", " ", "X", " ", " "}, TRUE},
+                {new String[] {"0", " ", " ", "0", " ", " ", "0", " ", " "}, TRUE},
+                {new String[] {" ", "X", " ", " ", "X", " ", " ", "X", " "}, TRUE},
+                {new String[] {" ", "0", " ", " ", "0", " ", " ", "0", " "}, TRUE},
+                {new String[] {" ", " ", "X", " ", " ", "X", " ", " ", "X"}, TRUE},
+                {new String[] {" ", " ", "0", " ", " ", "0", " ", " ", "0"}, TRUE},
+                {new String[] {"X", "0", "X", "0", "X", "X", "0", "X", "0"}, FALSE},
+                {new String[] {"X", " ", " ", "0", "X", " ", "0", "X", " "}, FALSE}
+        });
+    }
+
+    private String[] marks;
+    private boolean winnerLine;
+
+    public BoardLinesShould(String[] marks, boolean isWinnerLine) {
+        this.marks = marks;
+        this.winnerLine = isWinnerLine;
+    }
+
+    @Test public void
+    should_inform_when_there_is_a_winner_line() {
+        BoardLines boardLines = new BoardLines();
+
+        assertThat(boardLines.hasWinnerLine(marks), is(winnerLine));
+    }
+
+}
