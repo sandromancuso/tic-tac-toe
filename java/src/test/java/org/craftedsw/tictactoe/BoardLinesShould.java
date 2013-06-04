b8bf8444f0ea6f631da2a4ce989945a776b15eb7
Sandro Mancuso <sandromancuso@yahoo.com>
2013-06-04 09:42:30 +0100

    BoardLines test fixed

--- java/src/test/java/org/craftedsw/tictactoe/BoardLinesShould.java
+++ java/src/test/java/org/craftedsw/tictactoe/BoardLinesShould.java
@@ -1,9 +1,13 @@
 package org.craftedsw.tictactoe;
 
+import org.craftedsw.tictactoe.builder.MarksBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.craftedsw.tictactoe.Board.CELL_1;
+import static org.craftedsw.tictactoe.Board.CELL_5;
 import static org.craftedsw.tictactoe.BoardLines.ROW_2;
+import static org.craftedsw.tictactoe.builder.MarksBuilder.marks;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -20,7 +24,10 @@ public class BoardLinesShould {
 
     @Test public void
     should_return_null_when_there_is_no_winning_line() {
-        String[] marks = new String[]{"0", " ", " ", " ", " ", " ", "0", " ", " "};
+        String[] marks = marks()
+                               .fromPlayerOneAt(CELL_1, CELL_5)
+                               .fromPlayerTwoAt(Board.CELL_4, Board.CELL_9)
+                               .build();
 
         assertThat(boardLines.winningLine(marks),  is(nullValue()));
     }
