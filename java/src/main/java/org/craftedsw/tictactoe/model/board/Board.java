5745d39740676a198cdc2067dd6568b7ee183027
Sandro Mancuso <sandromancuso@yahoo.com>
2013-06-14 22:07:09 +0100

    Empty cells structure moved to BoardStructure

--- java/src/main/java/org/craftedsw/tictactoe/model/board/Board.java
+++ java/src/main/java/org/craftedsw/tictactoe/model/board/Board.java
@@ -3,12 +3,16 @@ package org.craftedsw.tictactoe.model.board;
 import org.craftedsw.tictactoe.model.game.Player;
 import org.craftedsw.tictactoe.view.BoardDisplay;
 
+import java.util.Arrays;
+
+import static java.util.Arrays.copyOf;
+import static org.craftedsw.tictactoe.model.board.BoardStructure.EMPTY_BOARD;
 import static org.craftedsw.tictactoe.model.game.Player.PLAYER_ONE;
 import static org.craftedsw.tictactoe.model.game.Player.PLAYER_TWO;
 
 public class Board {
 
-    private Marks marks = new Marks(new String[]{" ", " ", " ", " ", " ", " ", " ", " ", " "});
+    private Marks marks = new Marks(copyOf(EMPTY_BOARD, EMPTY_BOARD.length));
     private BoardLines boardLines = new BoardLines();
 
     private Player currentPlayer = PLAYER_ONE;
--- java/src/main/java/org/craftedsw/tictactoe/model/board/BoardStructure.java
+++ java/src/main/java/org/craftedsw/tictactoe/model/board/BoardStructure.java
@@ -2,6 +2,8 @@ package org.craftedsw.tictactoe.model.board;
 
 public interface BoardStructure {
 
+    String[] EMPTY_BOARD = new String[]{" ", " ", " ", " ", " ", " ", " ", " ", " "};
+
     int CELL_1 = 0;
     int CELL_2 = 1;
     int CELL_3 = 2;
@@ -24,11 +26,10 @@ public interface BoardStructure {
     Line DIAGONAL_2 = new Line(CELL_3, CELL_5, CELL_7);
 
     int[][] OPPOSITE_CORNER_CELLS = new int[][] {
-            {CELL_1, CELL_9},
-            {CELL_9, CELL_1},
-            {CELL_3, CELL_7},
-            {CELL_7, CELL_3},
-    };
+                                        {CELL_1, CELL_9},
+                                        {CELL_9, CELL_1},
+                                        {CELL_3, CELL_7},
+                                        {CELL_7, CELL_3}};
 
     int[] CORNER_CELLS = new int[] {CELL_1, CELL_3, CELL_7, CELL_9};
 
