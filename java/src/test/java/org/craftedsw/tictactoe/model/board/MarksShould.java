ea51d5bc19ffbf0dbfd9352c85032af7b24f0a16
Sandro Mancuso <sandromancuso@yahoo.com>
2013-07-17 21:17:41 +0100

    Missing test added. Was being tested via other tests before.

--- java/src/test/java/org/craftedsw/tictactoe/model/board/MarksShould.java
+++ java/src/test/java/org/craftedsw/tictactoe/model/board/MarksShould.java
@@ -5,6 +5,7 @@ import org.junit.experimental.theories.DataPoint;
 import org.junit.experimental.theories.Theories;
 import org.junit.runner.RunWith;
 
+import static org.craftedsw.tictactoe.builder.MarksBuilder.emptyMarks;
 import static org.craftedsw.tictactoe.model.board.BoardStructure.*;
 import static org.craftedsw.tictactoe.model.game.PlayerMark.CROSS;
 import static org.craftedsw.tictactoe.builder.MarksBuilder.marks;
@@ -14,7 +15,7 @@ import static org.junit.Assert.*;
 public class MarksShould {
 
     @Test public void
-    should_inform_when_it_is_not_full() {
+    inform_when_it_is_not_full() {
         Marks marks = marks()
                         .fromPlayerOneAt(CELL_1)
                         .fromPlayerTwoAt(CELL_9).build();
@@ -23,7 +24,7 @@ public class MarksShould {
     }
 
     @Test public void
-    should_inform_when_it_is_full() {
+    inform_when_it_is_full() {
         Marks marks = marks()
                             .fromPlayerOneAt(CELL_1, CELL_2, CELL_3, CELL_4, CELL_5)
                             .fromPlayerTwoAt(CELL_6, CELL_7, CELL_8, CELL_9)
@@ -33,21 +34,30 @@ public class MarksShould {
     }
 
     @Test public void
-    should_inform_which_is_the_first_empty_cell() {
+    inform_which_is_the_first_empty_cell() {
         Marks marks = marks().fromPlayerOneAt(CELL_1, CELL_2, CELL_7, CELL_9).build();
 
         assertThat(marks.firstEmptyCell(), is(CELL_3));
     }
 
     @Test(expected = RuntimeException.class) public void
-    should_throw_exception_when_mark_is_placed_on_a_marked_cell() {
+    throw_exception_when_mark_is_placed_on_a_marked_cell() {
         Marks marks = marks().fromPlayerOneAt(CELL_2).build();
 
         marks.placeMarkAt(CELL_2, CROSS.mark());
     }
 
     @Test public void
-    should_return_a_mark_at_specified_position() {
+    should_place_a_mark_on_specified_cell() {
+        Marks marks = emptyMarks();
+
+        marks.placeMarkAt(CELL_3, CROSS.mark());
+
+        assertThat(marks.markAt(CELL_3), is(CROSS.mark()));
+    }
+
+    @Test public void
+    return_a_mark_at_specified_position() {
         Marks marks = marks().fromPlayerOneAt(CELL_3).build();
 
         assertThat(marks.markAt(CELL_3), is(CROSS.mark()));
@@ -61,6 +71,16 @@ public class MarksShould {
                         .build();
 
         assertThat(marks.firstEmptySideCell(), is(CELL_6));
+    }
+
+    @Test public void
+    not_return_any_empty_side_cells_when_they_all_have_marks() {
+        Marks marks = marks()
+                        .fromPlayerOneAt(CELL_2, CELL_6)
+                        .fromPlayerTwoAt(CELL_4, CELL_8)
+                        .build();
+
+        assertThat(marks.firstEmptySideCell(), is(NO_CELL));
     }
 
 }
